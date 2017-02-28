ce3d4dfcbb2601d4e4ed8eb3cc113a1a8a2b330a
Naugh <j.rodriguezbarr@alumnos.urjc.es>
2017-02-28 09:57:22 +0100

    Añadido los html en static para comenzar

--- deliciousFoodSpring/src/main/java/com/deliciousFood/Application.java
+++ deliciousFoodSpring/src/main/java/com/deliciousFood/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeliciousFoodSpringApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeliciousFoodSpringApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
