006351c498dded1680fab950307a2c9334686c49
Naugh <j.rodriguezbarr@alumnos.urjc.es>
2017-02-28 09:30:31 +0100

    Inicio proyecto spring

---
+++ deliciousFoodSpring/src/main/java/com/deliciousFood/DeliciousFoodSpringApplication.java
@@ -0,0 +1,12 @@
+package com.deliciousFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciousFoodSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliciousFoodSpringApplication.class, args);
+	}
+}
