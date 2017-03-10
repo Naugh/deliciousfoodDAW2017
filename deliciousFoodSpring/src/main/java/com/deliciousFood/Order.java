c012a8199afdda66fcc913c79bb2b022010be19b
Melandt <j.raton@alumnos.urjc.es>
2017-03-10 20:41:19 +0100

    Realización de Order.java

--- deliciousFoodSpring/src/main/java/com/deliciousFood/Order.java
+++ deliciousFoodSpring/src/main/java/com/deliciousFood/Order.java
@@ -8,10 +8,105 @@ import javax.persistence.Id;
 
 @Entity
 public class Order {
-	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
+	
+	private String name;
+	private String surnames;
+	private String adress;
+	private String phone;
+	private String postal;
+	
+	
+	
+	public Order(){
+		
+	}
+
+
+
+	public Order(String name, String surnames, String adress, String phone, String postal) {
+		super();
+		this.name = name;
+		this.surnames = surnames;
+		this.adress = adress;
+		this.phone = phone;
+		this.postal = postal;
+	}
+
+
+
+	public long getId() {
+		return id;
+	}
+
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getSurnames() {
+		return surnames;
+	}
+
+
+
+	public void setSurnames(String surnames) {
+		this.surnames = surnames;
+	}
+
+
+
+	public String getAdress() {
+		return adress;
+	}
+
+
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+
+	public String getPostal() {
+		return postal;
+	}
+
+
+
+	public void setPostal(String postal) {
+		this.postal = postal;
+	}
+	
 	
 
 }
--- deliciousFoodSpring/src/main/java/com/deliciousFood/OrderController.java
+++ deliciousFoodSpring/src/main/java/com/deliciousFood/OrderController.java
@@ -1,14 +1,18 @@
 package com.deliciousFood;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OrderController {
+	
 
 	@RequestMapping("/order")
 	public String orderList(Model model){
+		
+		
 		return "order";
 	}
 }
