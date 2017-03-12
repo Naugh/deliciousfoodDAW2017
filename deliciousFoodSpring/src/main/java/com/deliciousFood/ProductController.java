5de0be646bbe3389f99f87331fdc8f05a60523e2
Naugh <j.rodriguezbarr@alumnos.urjc.es>
2017-03-12 15:25:48 +0100

    Restaurante con sus productos

--- deliciousFoodSpring/src/main/java/com/deliciousFood/ProductController.java
+++ deliciousFoodSpring/src/main/java/com/deliciousFood/ProductController.java
@@ -1,15 +1,27 @@
 package com.deliciousFood;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
+	
+	@Autowired
+	ProductRepository productRepository;
+	
+	@Autowired
+	RestaurantRepository restaurantRepository;
 
-	@RequestMapping("/product")
-	public String productList(Model model){
+	@RequestMapping("/products/{id}")
+	public String productList(Model model,@PathVariable String id){
+		Long idR = Long.parseLong(id);
+		Restaurant r = restaurantRepository.findById(idR);
+		model.addAttribute("products", r.getProducts());
 		return "products";
 	}
-	
 }
--- deliciousFoodSpring/src/main/java/com/deliciousFood/RestaurantRepository.java
+++ deliciousFoodSpring/src/main/java/com/deliciousFood/RestaurantRepository.java
@@ -1,12 +1,10 @@
 package com.deliciousFood;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long>{
 	
-	List<Product> findById(long id);
+	Restaurant findById(long id);
 
 }
 
