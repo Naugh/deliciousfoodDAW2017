f617169c7d3920859e0b17c93759ca6bbbb66c52
Melandt <j.raton@alumnos.urjc.es>
2017-03-21 02:03:36 +0100

    Retoques en html e implementación de la lógica que lleva a un usuario a editar sus datos

--- deliciousFoodSpring/src/main/java/com/deliciousFood/ProfileController.java
+++ deliciousFoodSpring/src/main/java/com/deliciousFood/ProfileController.java
@@ -5,11 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class FormController {
+public class ProfileController {
 
-	@RequestMapping("/form")
-	public String signUp(Model model) {
+	@RequestMapping("/editProfile")
+	public String editProfile(Model model) {
 		
-		return "form";
+		return "editProfile";
 	}
 }
--- deliciousFoodSpring/src/main/java/com/deliciousFood/UserController.java
+++ deliciousFoodSpring/src/main/java/com/deliciousFood/UserController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.tomcat.util.codec.binary.Base64;
@@ -46,9 +47,56 @@ public class UserController {
 		return "index";
 	}
 	
+	@RequestMapping("/editUser")
+	public String editUser(Model model, @RequestParam String userEmail,
+			@RequestParam String password, 
+			@RequestParam String userName, @RequestParam String userSurname,
+			@RequestParam String phone, @RequestParam String address, 
+			@RequestParam String postalCode, @RequestParam (required = false) String[] categories,
+			@RequestParam (required = false) MultipartFile image, HttpServletRequest request) throws IOException{
+		
+		if (request.isUserInRole("ROLE_RESTAURANT")) {
+			Restaurant r = restaurantRepository.findByEmail(request.getUserPrincipal().getName());
+			r.setPassword(password);
+			r.setPhone(phone);
+			r.setAddress(address);
+			r.setPostalCode(postalCode);
+			r.setName(userName);
+			r.setEmail(userEmail);
+			r.setImage(new String(Base64.encodeBase64(image.getBytes())));
+			Map<String, String> cat = new HashMap<String, String>();
+			for(int i= 0; i<categories.length;i++){
+				cat.put(categories[i].toUpperCase(), categories[i].toLowerCase());
+			}
+			r.setCategories(cat);
+			
+			restaurantRepository.save(r);
+		
+		}
+		
+		else if (request.isUserInRole("ROLE_PERSON")) {
+			Person p = personRepository.findByEmail(request.getUserPrincipal().getName());
+			p.setAddress(address);
+			p.setName(userName);
+			p.setSurnames(userSurname);
+			p.setPassword(password);
+			p.setPhone(phone);
+			p.setPostalCode(postalCode);
+			p.setEmail(userEmail);
+			
+			personRepository.save(p);
+		}
+			
+		
+		
+		return "index";
+	}
+	
+	
 	@RequestMapping("/loginUser")
 	public String loginPage(HttpSession session) {
 
 		return "login";
 	}
+	
 }
