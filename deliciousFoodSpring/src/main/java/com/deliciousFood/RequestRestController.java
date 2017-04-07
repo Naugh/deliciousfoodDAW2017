eafcf164d10fec893b0816b7cdc925ddabeed85d
Melandt <j.raton@alumnos.urjc.es>
2017-04-07 02:31:58 +0200

    User REST y Request Rest aunque hay errores

---
+++ deliciousFoodSpring/src/main/java/com/deliciousFood/RequestRestController.java
@@ -0,0 +1,59 @@
+package com.deliciousFood;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class RequestRestController {
+	
+	@Autowired
+	RequestRepository requestRepository;
+	
+	@RequestMapping(value = "/request", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	 public Request addRequest(@RequestBody Request request) {
+		
+		
+		requestRepository.save(request);
+
+		
+		return request;
+		
+	}
+	
+	@RequestMapping(value = "/request/{id}", method = RequestMethod.GET)
+	 public ResponseEntity<Request> getRequest(@PathVariable long id) {
+			
+		Request req =  requestRepository.findById(id);
+		if (req != null){
+			return new ResponseEntity<>(req, HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	
+	}
+	
+	
+	@RequestMapping(value = "/request/{id}", method = RequestMethod.DELETE)
+	 public ResponseEntity<Request> deleteRequest(@PathVariable long id) {
+	
+		Request req= requestRepository.findById(id);
+		if (req != null){			
+			requestRepository.delete(req);
+			return new ResponseEntity<>(req, HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+	}
+	
+
+}
--- deliciousFoodSpring/src/main/java/com/deliciousFood/UserRestController.java
+++ deliciousFoodSpring/src/main/java/com/deliciousFood/UserRestController.java
@@ -1,7 +1,13 @@
 package com.deliciousFood;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -41,4 +47,81 @@ public class UserRestController {
 		
 		return restaurant;
 	}
+	
+	
+	
+	@RequestMapping(value = "/person/{id}", method = RequestMethod.GET)
+	 public ResponseEntity<Person> getPerson(@PathVariable long id, HttpServletRequest request) throws IOException {
+
+		if (request.isUserInRole("ROLE_PERSON")) {
+			
+			Person p = personRepository.findById(id);
+			if (p != null){
+				return new ResponseEntity<>(p, HttpStatus.OK);
+			}
+			
+		}
+		
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+	}
+	
+
+	@RequestMapping(value = "/restaurant/{id}", method = RequestMethod.GET)
+	 public ResponseEntity<Restaurant> getRestaurant(@PathVariable long id, HttpServletRequest request) throws IOException{
+		
+		if (request.isUserInRole("ROLE_RESTAURANT")) {
+			
+			Restaurant r = restaurantRepository.findById(id);
+			if (r != null){
+				return new ResponseEntity<>(r, HttpStatus.OK);
+			}
+		
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+	}
+	
+	
+	
+	
+	@RequestMapping(value = "/person/{id}", method = RequestMethod.DELETE)
+	 public ResponseEntity<Person> deletePerson(@PathVariable long id, HttpServletRequest request) throws IOException {
+
+		if (request.isUserInRole("ROLE_PERSON")) {
+			
+			Person p = personRepository.findById(id);
+			if (p != null){
+				
+				personRepository.delete(p);
+				return new ResponseEntity<>(p, HttpStatus.OK);
+			}
+			
+		}
+		
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@RequestMapping(value = "/restaurant/{id}", method = RequestMethod.DELETE)
+	 public ResponseEntity<Restaurant> deleteRestaurant(@PathVariable long id, HttpServletRequest request) throws IOException{
+		
+		if (request.isUserInRole("ROLE_RESTAURANT")) {
+			
+			Restaurant r = restaurantRepository.findById(id);
+			if (r != null){
+				
+				restaurantRepository.delete(r);
+				return new ResponseEntity<>(r, HttpStatus.OK);
+			}
+		
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+	}
+	
+	
+	
+	
+	
 }
