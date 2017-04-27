588a12cb10c095ac09688e27e35222e40c7a6290
naugh <j.rodriguezbarr@alumnos.urjc.es>
2017-04-27 17:28:05 +0200

    Creando componente restaurantList (incompleto)

--- deliciousFoodAngular/src/app/app.routing.ts
+++ deliciousFoodAngular/src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { IndexComponent } from './index.component';
+import { RestaurantListComponent } from './restaurantList.component';
 
 const appRoutes = [
-    { path: '', component: IndexComponent }
+    { path: '', component: IndexComponent },
+    { path: 'restaurantList', component: RestaurantListComponent }
 ]
 
 export const routing = RouterModule.forRoot(appRoutes);
---
+++ deliciousFoodAngular/src/app/restaurantList.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+    moduleId: module.id,
+    selector: 'restaurantList',
+    templateUrl: 'restaurantList.template.html'
+})
+
+export class RestaurantListComponent{
+    constructor() { }
+}
