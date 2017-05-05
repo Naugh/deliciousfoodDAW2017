ec0795f14588248525074f510347ac38dcbb7b72
naugh <j.rodriguezbarr@alumnos.urjc.es>
2017-05-05 11:22:44 +0200

    Muestra imagenes, falta boton cargar mas

--- deliciousFoodAngular/src/app/restaurantList.component.ts
+++ deliciousFoodAngular/src/app/restaurantList.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit} from '@angular/core';
 import { Restaurant} from './restaurant.model';
 import { Router, ActivatedRoute } from '@angular/router';
 import { RestaurantService } from './restaurant.service';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     moduleId: module.id,
@@ -14,24 +13,19 @@ export class RestaurantListComponent implements OnInit{
 
     restaurants: Restaurant[];
 
-    constructor(private restaurantService: RestaurantService, private sanitizer:DomSanitizer) { }
+    constructor(private restaurantService: RestaurantService, private page) {
+        page=1;
+     }
 
     ngOnInit(){
-      this.restaurantService.getRestaurants(1).subscribe(
-        restaurants => this.loadRestaurants(restaurants),
-        //restaurants => console.log(restaurants),
-        error => console.log(error)
-      );
-       console.log(this.restaurants);
-
-     
-      console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+      this.loadRestaurants();
     }
 
-    loadRestaurants(restaurants){
-      this.restaurants = restaurants;
-      for(var i=0;i<restaurants.length;i++){
-        this.restaurants[i].imageSafe = this.sanitizer.bypassSecurityTrustUrl(this.restaurants[i].image);
-      }
+    loadRestaurants(){
+      this.restaurantService.getRestaurants(this.page).subscribe(
+        restaurants => this.restaurants.concat(restaurants),
+        error => console.log(error)
+      );
+      this.page++;
     }
 }
