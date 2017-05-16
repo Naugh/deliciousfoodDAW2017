bdae6669e942bd4a694ff55de71d33b54a3c8d84
Melandt <j.raton@alumnos.urjc.es>
2017-05-16 18:52:28 +0200

    editar perfil completado

--- deliciousFoodAngular/src/app/profile.component.ts
+++ deliciousFoodAngular/src/app/profile.component.ts
@@ -4,6 +4,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { RestaurantService } from './restaurant.service';
 import { LoginService } from './login.service';
 import { ProductsComponent } from './products.component';
+import { Person } from './person.model';
+
 
 
 @Component({
@@ -15,14 +17,41 @@ import { ProductsComponent } from './products.component';
 
 export class ProfileComponent implements OnInit{
 
-    
+    private person: Person;
+    private name: string;
+    private surnames: string;
+    private password: string;
+    private email: string;
+    private address: string;
+    private phone: string;
+    private postalCode: string;
 
-    constructor () {}
+    constructor ( private router: Router) {}
 
     ngOnInit(){
-
+        this.person = JSON.parse(sessionStorage.getItem("person"));
+        this.name = this.person.name;
+        this.surnames= this.person.surnames;
+        this.password= this.person.password;
+        this.email= this.person.email;
+        this.address= this.person.address;
+        this.phone= this.person.phone;
+        this.postalCode= this.person.postalCode;
     }
 
+    saveData(){
+        
+        this.person.name = this.name;
+        this.person.surnames= this.surnames;
+        this.person.password= this.password;
+        this.person.email= this.email;
+        this.person.address= this.address;
+        this.person.phone= this.phone;
+        this.person.postalCode= this.postalCode;
+        
+        sessionStorage.setItem("person", JSON.stringify(this.person));
+        this.router.navigate(['/']);
 
+    }
 
 }
